b8a8a37d1772e6f56c1d01ef32f3a5e42042dd98
Ahmed Ansari <cokeastrohero2011@gmail.com>
2025-11-16 18:41:24 +0300

    path parameters

--- main.py
+++ main.py
@@ -4,4 +4,19 @@ app = FastAPI()
 
 @app.get("/health_check")
 def health_check():
-    return "FastAPI is running properly"
+    return {"message": "FastAPI is running properly"}
+
+
+# path parameters
+
+@app.get("/items/{item_id}")
+def items(item_id:int):
+    return {"data":f"{item_id}"}
+
+# multiple path parameters
+@app.get("/items/{item_id}/{item_name}")
+def items(item_id:int, item_name:str):
+    return {
+        "item_name":item_name,
+        "item_id": item_id
+    }
